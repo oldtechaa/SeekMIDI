ad863b82ce52e14e3cec8691bb50fda81ec48aca
oldtechaa <oldtechaa@gmail.com>
2016-02-25 12:22:15 +0000

    Added some functionality to reach Milestone 2 - reading an event list from a file is now supported.

--- SeekMIDI.pl
+++ SeekMIDI.pl
@@ -19,53 +19,75 @@ sub midiWrite {
 	$midiPiece->write_to_file($midiFile);
 };
 
-my $events = [
-	['patch_change',    0, 0,  19],
-	['patch_change',    0, 1, 124],
-	[     'note_on',    0, 0,  50, 64],
-	[     'note_on',    0, 1,  50, 96],
-	[    'note_off',   96, 0,  50, 64],
-	[     'note_on',   96, 0,  49, 64],
-	[    'note_off',   96, 1,  50, 96],
-	[     'note_on',   96, 1,  49, 90],
-	[    'note_off',  192, 0,  48, 64],
-	[     'note_on',  192, 0,  49, 64],
-	[    'note_off',  192, 1,  48, 90],
-	[     'note_on',  192, 1,  49, 90],
-	[    'note_off',  288, 0,  48, 64],
-	[     'note_on',  288, 0,  49, 64],
-	[    'note_off',  288, 1,  48, 90],
-	[     'note_on',  288, 1,  49, 90],
-	[    'note_off',  384, 0,  48, 64],
-	[     'note_on',  384, 0,  49, 64],
-	[    'note_off',  384, 1,  48, 90],
-	[     'note_on',  384, 1,  49, 90],
-	[    'note_off',  480, 0,  48, 64],
-	[     'note_on',  480, 0,  49, 64],
-	[    'note_off',  480, 1,  48, 90],
-	[     'note_on',  480, 1,  49, 90],
-	[    'note_off',  576, 0,  48, 64],
-	[     'note_on',  576, 0,  49, 64],
-	[    'note_off',  576, 1,  48, 90],
-	[     'note_on',  576, 1,  49, 90],
-	[    'note_off',  672, 0,  48, 64],
-	[     'note_on',  672, 0,  49, 64],
-	[    'note_off',  672, 1,  48, 90],
-	[     'note_on',  672, 1,  49, 90],
-	[    'note_off',  768, 0,  48, 64],
-	[     'note_on',  768, 0,  49, 64],
-	[    'note_off',  768, 1,  48, 90],
-	[     'note_on',  768, 1,  49, 90],
-	[    'note_off',  864, 0,  48, 64],
-	[     'note_on',  864, 0,  49, 64],
-	[    'note_off',  864, 1,  48, 90],
-	[     'note_on',  864, 1,  49, 90],
-	[    'note_off',  960, 0,  48, 64],
-	[     'note_on',  960, 0,  49, 64],
-	[    'note_off',  960, 1,  48, 90],
-	[     'note_on',  960, 1,  49, 90],
-	[    'note_off', 1056, 0,  48, 64],
-	[    'note_off', 1056, 1,  48, 90]
-];
+# reads from the event file (future capability, not sure if I'll add it or not) This is just for testing, to reach Milestone2.
+sub evtOpen {
+	my $evtFile = shift;
+	open(my $evtHandle, "<", $evtFile);
+	
+	my @evtLines = <$evtHandle>;
+	return \@evtLines;
+};
 
-midiWrite($events, 1056,"Milestone1Test.mid");
+my @evtLines = @{evtOpen("events.in")};
+my @events;
+
+foreach (@evtLines) {
+	push(@events, [split(/,\s*/, $_)]);
+};
+
+# This can be changed around to reflect whatever type of track and file we need
+midiWrite(\@events, 96, "Milestone1Test2.mid");
+
+#my $events = [
+#	['patch_change',    0, 0,  19],
+#	['patch_change',    0, 1, 124],
+#	[     'note_on',    0, 0,  50, 64],
+#	[     'note_on',    0, 1,  50, 96],
+#	[    'note_off',   96, 0,  50, 64],
+#	[     'note_on',   96, 0,  49, 64],
+#	[    'note_off',   96, 1,  50, 96],
+#	[     'note_on',   96, 1,  49, 90],
+#	[    'note_off',  192, 0,  48, 64],
+#	[     'note_on',  192, 0,  49, 64],
+#	[    'note_off',  192, 1,  48, 90],
+#	[     'note_on',  192, 1,  49, 90],
+#	[    'note_off',  288, 0,  48, 64],
+#	[     'note_on',  288, 0,  49, 64],
+#	[    'note_off',  288, 1,  48, 90],
+#	[     'note_on',  288, 1,  49, 90],
+#	[    'note_off',  384, 0,  48, 64],
+#	[     'note_on',  384, 0,  49, 64],
+#	[    'note_off',  384, 1,  48, 90],
+#	[     'note_on',  384, 1,  49, 90],
+#	[    'note_off',  480, 0,  48, 64],
+#	[     'note_on',  480, 0,  49, 64],
+#	[    'note_off',  480, 1,  48, 90],
+#	[     'note_on',  480, 1,  49, 90],
+#	[    'note_off',  576, 0,  48, 64],
+#	[     'note_on',  576, 0,  49, 64],
+#	[    'note_off',  576, 1,  48, 90],
+#	[     'note_on',  576, 1,  49, 90],
+#	[    'note_off',  672, 0,  48, 64],
+#	[     'note_on',  672, 0,  49, 64],
+#	[    'note_off',  672, 1,  48, 90],
+#	[     'note_on',  672, 1,  49, 90],
+#	[    'note_off',  768, 0,  48, 64],
+#	[     'note_on',  768, 0,  49, 64],
+#	[    'note_off',  768, 1,  48, 90],
+#	[     'note_on',  768, 1,  49, 90],
+#	[    'note_off',  864, 0,  48, 64],
+#	[     'note_on',  864, 0,  49, 64],
+#	[    'note_off',  864, 1,  48, 90],
+#	[     'note_on',  864, 1,  49, 90],
+#	[    'note_off',  960, 0,  48, 64],
+#	[     'note_on',  960, 0,  49, 64],
+#	[    'note_off',  960, 1,  48, 90],
+#	[     'note_on',  960, 1,  49, 90],
+#	[    'note_off', 1056, 0,  48, 64],
+#	[    'note_off', 1056, 1,  48, 90]
+#];
+
+#midiWrite($events, 96, "Milestone1Test.mid");
+
+# The range for negative method ticks is 57857 - 59647
+# (($frames & 255) << 8) | $tpf
