5ebbfb2915620566b6e43448bdaa84ee3a5cefb4
oldtechaa <oldtechaa@gmail.com>
2017-06-23 14:31:50 -0400

    Changed license.
    As all code in SeekMIDI has been committed by oldtechaa, a change of license to make the licensing more compatible with the CPAN has been effected. This will also allow the use of the SeekMIDI MIDI graph widget in other applications if split off as a module.

--- bin/SeekMIDI.pl
+++ bin/SeekMIDI.pl
@@ -1,20 +1,9 @@
 #!/usr/bin/perl
 
 # SeekMIDI, a simple graphical MIDI sequencer
-# Copyright (C) 2017 oldtechaa  <oldtechaa@gmail.com>
+# This software is copyright (c) 2017 by oldtechaa <oldtechaa@gmail.com>.
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
 
 # ALWAYS MAINTAIN NO WARNINGS AND NO ERRORS!
 use warnings;
